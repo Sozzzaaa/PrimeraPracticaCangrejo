27b8f32f47538c11800dd5c77bf0a2b1d49425fe
Sozzzaaa <sosa.santana.luisemiliano@gmail.com>
2025-09-23 19:24:22 -0600

    my life be like uuwa uwa uwauuuuuuuuuuuUUu m

--- app/src/main/java/com/example/primerapracticacangrejo/view/LoginActivity.kt
+++ app/src/main/java/com/example/primerapracticacangrejo/view/LoginActivity.kt
@@ -6,13 +6,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.primerapracticacangrejo.R
+import com.example.primerapracticacangrejo.databinding.ActivityMainBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = activit
         enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.textViewIngCorreo.text.toString()
+            val password = binding.textViewIngCont.text.toString()
+            // TODO: validate login
+        }
+
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
